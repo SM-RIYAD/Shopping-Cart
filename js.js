62e11f3bf087b12909194ff730794e56346a07c5
SM-RIYAD <88622536+SM-RIYAD@users.noreply.github.com>
2022-02-11 20:57:50 +0600

    handled an exceptional case,wasn't necessary though!!

--- js.js
+++ js.js
@@ -4,51 +4,93 @@ var subtotalNum = getNum("subtotalId");
 var totalNum = getNum("totalId");
 
 
-console.log("subtotal num ", subtotalNum)
+
 const mobileAdd = document.getElementById("mobile-plus-btn");
 mobileAdd.addEventListener("click", function() {
-    PlusOrMinus("mobile-quantity", 1);
-    multiplyOrDiv("mobile-price", initialMobilePriceNum);
+    mobileQuantityNum = getNumValue("mobile-quantity");
+
+    mobilePriceNum = getNum("mobile-price");
+    PlusOrMinus("mobile-quantity", mobileQuantityNum, 1);
+    multiplyOrDiv("mobile-price", mobilePriceNum, initialMobilePriceNum);
 
 })
 const mobileMinus = document.getElementById("mobile-minus-btn");
 mobileMinus.addEventListener("click", function() {
-    PlusOrMinus("mobile-quantity", -1);
-    multiplyOrDiv("mobile-price", -1 * initialMobilePriceNum);
+
+    mobileQuantityNum = getNumValue("mobile-quantity");
+    mobilePriceNum = getNum("mobile-price");
+    if (mobileQuantityNum > 0) {
+        PlusOrMinus("mobile-quantity", mobileQuantityNum, -1);
+
+        multiplyOrDiv("mobile-price", mobilePriceNum, -1 * initialMobilePriceNum);
+    }
+
+
+
 })
 const caseAdd = document.getElementById("case-plus-btn");
 caseAdd.addEventListener("click", function() {
-    PlusOrMinus("case-quantity", 1);
-    multiplyOrDiv("case-price", initialCasePriceNum);
+
+
+
+    caseQuantityNum = getNumValue("case-quantity");
+    casePriceNum = getNum("case-price");
+    PlusOrMinus("case-quantity", caseQuantityNum, 1);
+    multiplyOrDiv("case-price", casePriceNum, initialCasePriceNum);
+
 })
 const CaseMinus = document.getElementById("case-minus-btn");
 CaseMinus.addEventListener("click", function() {
-    PlusOrMinus("case-quantity", -1);
-    multiplyOrDiv("case-price", -1 * initialCasePriceNum);
+
+    caseQuantityNum = getNumValue("case-quantity");
+    casePriceNum = getNum("case-price");
+    if (caseQuantityNum > 0) {
+
+        PlusOrMinus("case-quantity", caseQuantityNum, -1);
+
+        multiplyOrDiv("case-price", casePriceNum, -1 * initialCasePriceNum);
+    }
+
 })
 
-function PlusOrMinus(id, n) {
-    var Quantity = document.getElementById(id).value;
-    var QuantityNum = parseFloat(Quantity);
-    var total = QuantityNum + n;
-    console.log(QuantityNum, n)
+function PlusOrMinus(id, x, n) {
+
+
+
+    var total = x + n;
+
     document.getElementById(id).value = total;
+
+
 }
 
-function multiplyOrDiv(id, n) {
-    var Price = document.getElementById(id).innerText;
-    var PriceNum = parseFloat(Price);
+function multiplyOrDiv(id, y, n) {
+
+    var PriceNum = y;
+
     var total = PriceNum + n;
-    console.log(subtotalNum, n);
+
     subtotalNum = subtotalNum + n;
     document.getElementById("subtotalId").innerText = subtotalNum;
     document.getElementById("totalId").innerText = subtotalNum;
+
     document.getElementById(id).innerText = total;
+
+
+
 }
 
 function getNum(id) {
 
     var idName = document.getElementById(id).innerText;
+    return parseFloat(idName);
+
+
+}
+
+function getNumValue(id) {
+
+    var idName = document.getElementById(id).value;
     return parseFloat(idName);
 
 
